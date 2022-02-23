eda537b26548fa0935893967efbd7f2e1abb45ad
sh-hay <hayashi-shunsuke@hro.or.jp>
2022-02-23 23:19:09 +0900

    update .ino to control one motor

--- simple_motor_control/simple_motor_control.ino
+++ simple_motor_control/simple_motor_control.ino
@@ -1,55 +1,41 @@
-#define motorPinA 8
-#define motorPinB 9
-#define motorPWMpin 10
+// #define motorPinA 8
+// #define motorPinB 9
+#define motorPWMpinA1 5
+#define motorPWMpinA2 6
+#define motorPWMpinB1 7
+#define motorPWMpinB2 8
 
-const int analogPin = 0; // ポテンショメータのワイプ(中央の端子)を接続する
-                       // 両端はグランドと+5Vに接続
-int val = 0;           // 読み取った値を格納する変数
+const int analogPin = 0; 
+int val = 0;           
+const int vol_lim = 800;
+const int vol_mid = vol_lim/2;
+const int dead_range_half = 20;
 
 void setup() {
-  pinMode(motorPinA, OUTPUT);
-  pinMode(motorPinB, OUTPUT);
-  Serial.begin(115200);        // シリアル通信の初期化
+  Serial.begin(115200);  
 }
 
 void loop() {
   val = analogRead(analogPin);    // アナログピンを読み取る
-  if(val > 800) val = 800;
-  val = map(val,0,800,0,255);
-  Serial.println(val);
-  // delay(200);
-  digitalWrite( motorPinA, HIGH );
-  digitalWrite( motorPinB, LOW  );
-  analogWrite( motorPWMpin, val );
+  if(val > vol_lim) val = vol_lim;
+  int pwm_val = map(val,0,vol_lim,0,255);
+
+  if( val>(vol_mid+dead_range_half) ){
+    analogWrite( motorPWMpinA1, pwm_val*2-255 );
+    analogWrite( motorPWMpinA2, 0 );
+  }
+  else if( val<(vol_mid-dead_range_half) ){
+    analogWrite( motorPWMpinA1, 0 );
+    analogWrite( motorPWMpinA2, 255-pwm_val*2 );
+  }
+  else{
+    analogWrite( motorPWMpinA1, 0 );
+    analogWrite( motorPWMpinA2, 0 );
+  }
+  
+  Serial.print(val);
+  Serial.print("\t");
+  Serial.print(pwm_val);
+  Serial.print("\n");
 }
 
-// #define motorPinA 7
-// #define motorPinB 8
-// #define motorPWMpin  9
-
-
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   pinMode(motorPinA, OUTPUT);
-//   pinMode(motorPinB, OUTPUT);
-  
-
-// }
-
-// void loop() {
-//   int pwm_val = analogRead()
-//   digitalWrite( motorPinA, HIGH );
-//   digitalWrite( motorPinB, LOW  );
-//   analogWrite( motorPWMpin, 100 );
-//   delay( 3000 );
-//   analogWrite( motorPWMpin, 0 );
-//   delay( 1000 );
-
-//   digitalWrite( motorPinA, LOW );
-//   digitalWrite( motorPinB, HIGH  );
-//   analogWrite( motorPWMpin, 100 );
-//   delay( 1000 );
-//   analogWrite( motorPWMpin, 0 );
-//   delay( 3000 );
-// }
