789be64069b5ebea1ae97ada9528665cb6f6a2e0
sh-hay <hayashi-shunsuke@hro.or.jp>
2022-02-24 20:43:58 +0900

    dual motor channel

--- simple_motor_control/simple_motor_control.ino
+++ simple_motor_control/simple_motor_control.ino
@@ -1,12 +1,13 @@
-// #define motorPinA 8
-// #define motorPinB 9
 #define motorPWMpinA1 5
 #define motorPWMpinA2 6
 #define motorPWMpinB1 7
 #define motorPWMpinB2 8
 
-const int analogPin = 0; 
-int val = 0;           
+const int analogPinA = 0; 
+const int analogPinB = 1; 
+int analogValA = 0;          
+int analogValB = 0;          
+
 const int vol_lim = 800;
 const int vol_mid = vol_lim/2;
 const int dead_range_half = 20;
@@ -16,26 +17,51 @@ void setup() {
 }
 
 void loop() {
-  val = analogRead(analogPin);    // アナログピンを読み取る
-  if(val > vol_lim) val = vol_lim;
-  int pwm_val = map(val,0,vol_lim,0,255);
+  // モーターA
+  // アナログの入力1024（900くらいまでしか上がらない）を255に
+  analogValA = analogRead(analogPinA);
+  if(analogValA > vol_lim) analogValA = vol_lim;
+  int pwmValA = map(analogValA,0,vol_lim,0,255);
 
-  if( val>(vol_mid+dead_range_half) ){
-    analogWrite( motorPWMpinA1, pwm_val*2-255 );
+  if( analogValA>(vol_mid+dead_range_half) ){
+    analogWrite( motorPWMpinA1, pwmValA*2-255 );
     analogWrite( motorPWMpinA2, 0 );
   }
-  else if( val<(vol_mid-dead_range_half) ){
+  else if( analogValA<(vol_mid-dead_range_half) ){
     analogWrite( motorPWMpinA1, 0 );
-    analogWrite( motorPWMpinA2, 255-pwm_val*2 );
+    analogWrite( motorPWMpinA2, 255-pwmValA*2 );
   }
   else{
     analogWrite( motorPWMpinA1, 0 );
     analogWrite( motorPWMpinA2, 0 );
   }
-  
-  Serial.print(val);
+
+  // モーターB
+  analogValB = analogRead(analogPinB);
+  if(analogValB > vol_lim) analogValB = vol_lim;
+  int pwmValB = map(analogValB,0,vol_lim,0,255);
+
+  if( analogValB>(vol_mid+dead_range_half) ){
+    analogWrite( motorPWMpinB1, pwmValB*2-255 );
+    analogWrite( motorPWMpinB2, 0 );
+  }
+  else if( analogValB<(vol_mid-dead_range_half) ){
+    analogWrite( motorPWMpinB1, 0 );
+    analogWrite( motorPWMpinB2, 255-pwmValB*2 );
+  }
+  else{
+    analogWrite( motorPWMpinB1, 0 );
+    analogWrite( motorPWMpinB2, 0 );
+  }
+
+  // デバッグ用
+  Serial.print(analogValA);
   Serial.print("\t");
-  Serial.print(pwm_val);
+  Serial.print(analogValB);
+  Serial.print("\t");
+  Serial.print(pwmValA);
+  Serial.print("\t");
+  Serial.print(pwmValB);
   Serial.print("\n");
 }
 
